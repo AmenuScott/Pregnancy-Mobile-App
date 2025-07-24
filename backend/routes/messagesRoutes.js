0c4925b2e5b52076f8ddedcaf5ae627afdeab642
AmenuScott <amenusamuel77@gmail.com>
2025-07-24 13:02:49 +0000

    committed

--- backend/routes/messagesRoutes.js
+++ backend/routes/messagesRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const messageController = require("../controllers/messageController");
+const messageController = require("../controllers/messagesController");
 
 router.post("/send", messageController.sendMessage);
 router.get("/thread/:senderId/:receiverId", messageController.getMessageThread);
