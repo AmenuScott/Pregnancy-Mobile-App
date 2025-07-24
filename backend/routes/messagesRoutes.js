a7927154d70b0fff572ebc856a66898762537a69
AmenuScott <amenusamuel77@gmail.com>
2025-07-24 13:18:41 +0000

    commit

--- backend/routes/messagesRoutes.js
+++ backend/routes/messagesRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const messageController = require("../controllers/messagesController");
+const messagesController = require("../controllers/messagesController");
 
-router.post("/send", messageController.sendMessage);
-router.get("/thread/:senderId/:receiverId", messageController.getMessageThread);
-router.get("/:userId", messageController.getUserMessages); // if you still use it
-router.get("/thread/:user1/:user2", messageController.getConversation);
-
+router.post("/send", messagesController.sendMessage);
+router.get("/thread/:senderId/:receiverId", messagesController.getMessageThread);
+router.get("/:userId", messagesController.getUserMessages); // if you still use it
+router.get("/thread/:user1/:user2", messagesController.getConversation);
 
 module.exports = router;
