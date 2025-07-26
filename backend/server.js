42ce3f9742aeb6af5919e4bb6bdf101f0eb2c0fc
AmenuScott <amenusamuel77@gmail.com>
2025-07-26 09:10:58 +0000

    commit

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/", patientRoutes);
 app.use("/api/setup", setupRoutes);
 app.use("/api/notifications", notificationRoutes);
-app.use("/api/tips", tipRoutes);
+app.use("/api/", tipRoutes);
 app.use("/api/exercises", exerciseRoutes);
 app.use("/api/emergency", emergencyRoutes);
 app.use("/api/messages", messageRoutes);
