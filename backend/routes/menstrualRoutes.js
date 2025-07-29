f62219af8d22d2b88dfc7673b9d04b7a8ab47ae9
AmenuScott <amenusamuel77@gmail.com>
2025-07-29 09:25:08 +0000

    m

--- backend/routes/menstrualRoutes.js
+++ backend/routes/menstrualRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { createMenstrualLog } = require("../controllers/menstrualController");
 const { getMenstrualLogs } = require("../controllers/menstrualController");
-const { getMenstrualInsights } = require("../controllers/menstrualController");
+const menstrualController = require("../controllers/menstrualController");
+
 
 router.post("/menstrual-logs", createMenstrualLog);
 router.get("/menstrual-logs/:user_id", getMenstrualLogs);
