935108684ff2d23965d6b5e4a6c23bb0013d1515
AmenuScott <amenusamuel77@gmail.com>
2025-07-31 10:29:09 +0000

    xgh

--- backend/app.config.js
+++ backend/app.config.js
@@ -2,9 +2,9 @@ import 'dotenv/config'
 
 export default {
   expo: {
-    name: "PregWell",
-    slug: "pregwell",
-    version: "1.0.0",
+    name: 'PregWell',
+    slug: 'pregwell',
+    version: '1.0.0',
     extra: {
       openaiApiKey: process.env.OPENAI_API_KEY,
     },
